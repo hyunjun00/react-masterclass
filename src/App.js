1ece0d4a93c9ff6ecf32da0b79712c79a7594962
hyunjun <khjae0207@naver.com>
2024-01-18 11:30:58 +0900

    Pseudo Selectors part Two

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper=styled.div`
   display:flex;
+  height:100vh;
+  width:100vw;
+  justify-content:center;
+  align-items:center;
 `;
 
 const rotationanimation=keyframes`
@@ -18,6 +22,10 @@ const rotationanimation=keyframes`
   }
 `;
 
+const Emoji=styled.span`
+  font-size:36px;
+`;
+
 const Box=styled.div`
   height:200px;
   width:200px;
@@ -26,14 +34,8 @@ const Box=styled.div`
   justify-content:center;
   align-items:center;
   animation:${rotationanimation} 1s linear infinite;
-  span {
-    font-size:36px;
-    &:hover {
-      font-size:40px;
-    }
-    &:active {
-      opacity:0;
-    }
+  ${Emoji}:hover {
+      font-size:98px;
   }
 `;
 
@@ -41,8 +43,9 @@ function App() {
   return (
     <Wrapper>
       <Box>
-        <span>😁</span>
+        <Emoji as="p">😁</Emoji>
       </Box>
+      <Emoji>🐉</Emoji>
     </Wrapper>
   )
 }
