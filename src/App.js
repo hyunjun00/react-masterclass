85befba8b1e390766c958d0ad6fb83f8e07bcdfe
hyunjun <khjae0207@naver.com>
2024-01-18 11:59:07 +0900

    Themes

--- src/App.js
+++ src/App.js
@@ -6,46 +6,17 @@ const Wrapper=styled.div`
   width:100vw;
   justify-content:center;
   align-items:center;
+  background-color:${props=>props.theme.backgroundColor};
 `;
 
-const rotationanimation=keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    border-radius:100px;
-  }
-  100% {
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji=styled.span`
-  font-size:36px;
-`;
-
-const Box=styled.div`
-  height:200px;
-  width:200px;
-  background-color:tomato;
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  animation:${rotationanimation} 1s linear infinite;
-  ${Emoji}:hover {
-      font-size:98px;
-  }
+const Title=styled.h1`
+  color:${(props)=>props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😁</Emoji>
-      </Box>
-      <Emoji>🐉</Emoji>
+      <Title>HELLO</Title>
     </Wrapper>
   )
 }
